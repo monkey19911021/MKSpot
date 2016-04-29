a178cabcb089d5d3398953b089aeb2fbf62e7e03
liujh <457870936@qq.com>
2016-04-29 10:01:54 +0800

    添加缓冲效果

--- MKSpot/MKSpotAnimation.swift
+++ MKSpot/MKSpotAnimation.swift
@@ -13,6 +13,7 @@ let PROCESS_DURING = 3.6
 let SPOT_DELAY_RATIO = 0.08
 
 let UNIT_RADIUS: CGFloat = 5 //spot半径
+let ti: CGFloat = 0.03
 
 
 class MKPreLoadSpot: UIView{
@@ -45,8 +46,13 @@ class MKPreLoad: UIView{
     private var margin: CGFloat = 0
     private var originX: CGFloat = 0
     private var finalX: CGFloat = 0
+    private var originRearX: CGFloat = 0
+    private var finalRearX: CGFloat = 0
     
-    
+    private var spotScaleVal1 = NSValue(CATransform3D: CATransform3DMakeScale(1.0, 1.0, 0))
+    private var spotScaleVal2 = NSValue(CATransform3D: CATransform3DMakeScale(2.0, 2.0, 0))
+    private var spotScaleVal3 = NSValue(CATransform3D: CATransform3DMakeScale(3.0, 3.0, 0))
+    private var spotScaleVal4 = NSValue(CATransform3D: CATransform3DMakeScale(4.0, 4.0, 0))
     
     init(){
         super.init(frame: CGRectZero)
@@ -57,7 +63,8 @@ class MKPreLoad: UIView{
         margin = frame.size.width/4
         originX = margin
         finalX = frame.size.width - margin;
-        
+        originRearX = originX - 4 * UNIT_RADIUS
+        finalRearX = finalX + 4 * UNIT_RADIUS
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -74,26 +81,88 @@ class MKPreLoad: UIView{
                          movingSoptView3]
         
         //左边固定点
-        leftSoptView = MKPreLoadSpot(frame: CGRectMake(originX - UNIT_RADIUS, frame.size.height/2 - UNIT_RADIUS, 2 * UNIT_RADIUS, 2 * UNIT_RADIUS), color: UIColor.greenColor())
-        self.addSubview(leftSoptView)
+        addLeftSpotAnimation()
         
         //右边固定点
-        rightSoptView = MKPreLoadSpot(frame: CGRectMake(finalX - UNIT_RADIUS, frame.size.height/2 - UNIT_RADIUS, 2 * UNIT_RADIUS, 2 * UNIT_RADIUS), color: UIColor.greenColor())
-        self.addSubview(rightSoptView)
+        addRightSpotAnimation()
         
         for (index,var spot) in spotArray.enumerate(){
-            spot = MKPreLoadSpot(frame: leftSoptView.frame, color: UIColor.greenColor());
+            spot = MKPreLoadSpot(frame: CGRectMake(originX - UNIT_RADIUS, frame.size.height/2 - UNIT_RADIUS, 2 * UNIT_RADIUS, 2 * UNIT_RADIUS), color: UIColor.greenColor());
             self.addSubview(spot)
             
             //添加关键帧动画
-            addKeyFrameAnimation(spot, index: index-1)
+            addKeyFrameAnimation(spot, index: index)
         }
     }
     
+    //MARK: 左边固定点动画
+    private func addLeftSpotAnimation(){
+        leftSoptView = MKPreLoadSpot(frame: CGRectMake(originX - UNIT_RADIUS, frame.size.height/2 - UNIT_RADIUS, 2 * UNIT_RADIUS, 2 * UNIT_RADIUS), color: UIColor.greenColor())
+        self.addSubview(leftSoptView)
+        
+        leftSoptView.layer.transform = CATransform3DMakeScale(4.0, 4.0, 0)
+        let ani = CAKeyframeAnimation(keyPath: "transform")
+        ani.values = [spotScaleVal3,
+                      spotScaleVal4,
+                      spotScaleVal4,
+                      spotScaleVal3,
+                      spotScaleVal3,
+                      spotScaleVal2,
+                      spotScaleVal2,
+                      spotScaleVal1,
+                      spotScaleVal1,
+                      spotScaleVal2,
+                      spotScaleVal2,
+                      spotScaleVal3,
+                      spotScaleVal3,]
+        ani.keyTimes = [0.0, 0.0 + ti, 0.25, 0.25 + ti, 0.33, 0.33 + ti, 0.41, 0.41 + ti, 0.84, 0.84 + ti, 0.92, 0.92 + ti, 1.00]//SPOT_DELAY_RATIO = 0.08
+        ani.duration = PROCESS_DURING
+        ani.repeatCount = MAXFLOAT
+        leftSoptView.layer.addAnimation(ani, forKey: "fixedSpotScaleAnim")
+    }
+    
+    //MARK: 右边固定点动画
+    private func addRightSpotAnimation(){
+        rightSoptView = MKPreLoadSpot(frame: CGRectMake(finalX - UNIT_RADIUS, frame.size.height/2 - UNIT_RADIUS, 2 * UNIT_RADIUS, 2 * UNIT_RADIUS), color: UIColor.greenColor())
+        self.addSubview(rightSoptView)
+        
+        let ani = CAKeyframeAnimation(keyPath: "transform")
+        ani.values = [spotScaleVal1,
+                      spotScaleVal1,
+                      spotScaleVal2,
+                      spotScaleVal2,
+                      spotScaleVal3,
+                      spotScaleVal3,
+                      spotScaleVal4,
+                      spotScaleVal4,
+                      spotScaleVal3,
+                      spotScaleVal3,
+                      spotScaleVal2,
+                      spotScaleVal2,
+                      spotScaleVal1,
+                      spotScaleVal1,]
+        ani.keyTimes = [0.0, 0.25, 0.25 + ti, 0.33, 0.33 + ti, 0.41, 0.41 + ti, 0.75, 0.75 + ti, 0.83, 0.83 + ti, 0.91, 0.91 + ti, 1.00]//SPOT_DELAY_RATIO = 0.08
+        ani.duration = PROCESS_DURING
+        ani.repeatCount = MAXFLOAT
+        //0.1 ratio needed that the spot from left to right
+        ani.beginTime = CACurrentMediaTime() + PROCESS_DURING * 0.1;
+        rightSoptView.layer.addAnimation(ani, forKey: "fixedSpotScaleAnim")
+    }
+    
+    //MARK: 移动点动画
     private func addKeyFrameAnimation(spot: UIView, index: Int){
         let anim = CAKeyframeAnimation(keyPath: "position.x")
-        anim.values = [originX, originX, finalX, finalX, originX, originX]
-        anim.keyTimes = [0.0, 0.25, 0.35, 0.75, 0.85, 1.0];//sleep 0.4 ratio
+        anim.values = [originX,
+                       originX,
+                       finalX,
+                       finalRearX,
+                       finalX,
+                       finalX,
+                       originX,
+                       originRearX,
+                       originX,
+                       originX]
+        anim.keyTimes = [0.0, 0.25, 0.35, 0.38, 0.41, 0.75, 0.85, 0.88, 0.91, 1.0];//sleep 0.4 ratio
         anim.duration = PROCESS_DURING
         anim.repeatCount = MAXFLOAT
         anim.beginTime = CACurrentMediaTime() + Double(index) * SPOT_DELAY_RATIO * PROCESS_DURING
